38080513dbaabad029702be115197f1a2245e479
Alfredo Bejarano <alfredocbejarano@gmail.com>
2018-10-23 10:15:07 -0500

    Create AppComponent.kt
    Class that defines the dependency graph for the project dependency injection

---
+++ app/src/main/java/me/alfredobejarano/tictactoe/utilities/AppComponent.kt
@@ -0,0 +1,33 @@
+package me.alfredobejarano.tictactoe.utilities
+
+import dagger.Component
+import me.alfredobejarano.tictactoe.data.ScoreboardDao
+import me.alfredobejarano.tictactoe.data.ScoreboardRepository
+import javax.inject.Singleton
+
+/**
+ *
+ * Class that defines the dependency injection graph for this project using Dagger.
+ *
+ * @author Alfredo Bejarano
+ * @since 23/10/18
+ * @version 1.0
+ **/
+@Singleton
+@Component(
+    modules = [
+        ScoreboardDaoModule::class,
+        ScoreboardRepositoryModule::class
+    ]
+)
+interface AppComponent {
+    /**
+     * Provides injection for the [ScoreboardDao] class.
+     */
+    fun provideScoreboardDao(): ScoreboardDao
+
+    /**
+     * Provides injection for the [ScoreboardRepository] class.
+     */
+    fun provideScoreboardRepository(): ScoreboardRepository
+}
