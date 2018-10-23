c204df1eb9f7653963457f3092bc415a83ef1f80
Alfredo Bejarano <alfredocbejarano@gmail.com>
2018-10-23 10:15:45 -0500

    Create Injector.kt
    Object that provides singleton access to the AppComponent implementation made by dagger, this allows performing injection for the classes that uses the @Inject annotation

---
+++ app/src/main/java/me/alfredobejarano/tictactoe/utilities/Injector.kt
@@ -0,0 +1,41 @@
+package me.alfredobejarano.tictactoe.utilities
+
+import android.app.Application
+
+/**
+ *
+ * [Write your documentation here]
+ *
+ * @author Alfredo Bejarano
+ * @since 23/10/18
+ * @version 1.0
+ **/
+object Injector {
+    /**
+     * Reference to this project [Application] class,
+     * this helps having a [Context] without having to
+     * keep it statically and have memory leaks because of that.
+     */
+    @Volatile
+    private lateinit var mApp: Application
+
+    /**
+     * Singleton access for the [component][AppComponent]
+     * dagger implementation for performing injection.
+     */
+    val component by lazy {
+        DaggerAppComponent
+            .builder()
+            .scoreboardDaoModule(ScoreboardDaoModule(mApp))
+            .scoreboardRepositoryModule(ScoreboardRepositoryModule())
+            .build()
+    }
+
+    /**
+     * Initializes this injector object, basically
+     * assigns a value to the mApp property of this class.
+     */
+    fun initialize(app: Application) {
+        mApp = app
+    }
+}
