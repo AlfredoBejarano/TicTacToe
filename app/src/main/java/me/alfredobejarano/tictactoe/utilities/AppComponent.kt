ecb5fa86c6f221f0b67361cb3403e02f840e6049
Alfredo Bejarano <alfredocbejarano@gmail.com>
2018-10-24 13:20:02 -0500

    Update AppComponent.kt
    Add the Inject function for the GameActivity to the AppComponent

--- app/src/main/java/me/alfredobejarano/tictactoe/utilities/AppComponent.kt
+++ app/src/main/java/me/alfredobejarano/tictactoe/utilities/AppComponent.kt
@@ -1,6 +1,7 @@
 package me.alfredobejarano.tictactoe.utilities
 
 import dagger.Component
+import me.alfredobejarano.tictactoe.GameActivity
 import me.alfredobejarano.tictactoe.data.ScoreboardDao
 import me.alfredobejarano.tictactoe.data.ScoreboardRepository
 import javax.inject.Singleton
@@ -22,6 +23,12 @@ import javax.inject.Singleton
     ]
 )
 interface AppComponent {
+    /**
+     * This function injects all the fields annotated with
+     * the @Inject annotation in the [GameActivity] class.
+     */
+    fun inject(activity: GameActivity)
+
     /**
      * Provides injection for the [ScoreboardDao] class.
      */
